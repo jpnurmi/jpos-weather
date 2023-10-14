cc1ba32e2a9491d5e9efd995076e1acb2cd72ec6
J-P Nurmi <jpnurmi@gmail.com>
2023-10-14 13:38:53 +0200

    Allow setting SLINT_STYLE for testing purposes

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
 fn main() {
+    let style = std::env::var("SLINT_STYLE").unwrap_or("fluent".into());
     slint_build::compile_with_config(
         "ui/main.slint",
-        slint_build::CompilerConfiguration::new().with_style("fluent".into()),
+        slint_build::CompilerConfiguration::new().with_style(style),
     )
     .unwrap();
 }
